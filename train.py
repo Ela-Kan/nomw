a5750ebf216423e46a79e88bafef1d13456a9c88
sophmrtn <sophiedarrouxmartin@gmail.com>
2023-11-10 13:03:22 +0000

    add validation loop

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from Discriminator import Discriminator
 from utils import wasserstein_loss, transform_V
 from Data_Loader import Dataset, prepare_data
 import matplotlib.pyplot as plt
-from monai.transforms import Compose,RandShiftIntensity, RandBiasField, RandScaleIntensity, RandAdjustContrast
+from monai.transforms import Compose,RandShiftIntensity, RandBiasField, RandScaleIntensity, RandAdjustContrast, ToNumpy
 
 batch_size = 8
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -18,14 +18,15 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Define your transformations accordingly
 
 """NOTE: The following transformations must be applied to a np array, but they output tensors
+"""
+
 intensity_transform = Compose([
+    ToNumpy(),
     RandShiftIntensity(offsets=100, prob = 1),  # Adjust intensity by scaling with a factor of 1.5
     RandBiasField(degree = 2, prob = 1),
     RandScaleIntensity(factors=0.5, prob = 1),
-    RandAdjustContrast(gamma = 2, prob = 1)
-])
+    RandAdjustContrast(gamma = 2, prob = 1)])
 
-"""
 # Create DataLoader
 subject_ids = prepare_data('data\mni')
 dataset = Dataset([subject_ids[:10]], 'data\mni', is_motion_corrected=True)
@@ -33,7 +34,7 @@ dataset = Dataset([subject_ids[:10]], 'data\mni', is_motion_corrected=True)
 # =============================================================================
 # Split into (training and validation datasets
 # =============================================================================
-train_set, val_set = random_split(dataset, [0.8, 0.2], generator=torch.Generator.manual_seed(0))
+train_set, val_set = random_split(dataset, [0.8, 0.2], generator=torch.Generator.manual_seed(seed=0))
 train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 val_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True)
 
@@ -77,8 +78,6 @@ for epoch in range(num_epochs):
         # Train Generator
         optimizer_G.zero_grad()
         gen_images = gan.generator(filtered_images, subject_ids)
-        # print(gen_images.shape)
-        # print(plt.imshow(gen_images[0,:,:,:,45].detach().numpy()[0], cmap='gray'))
         g_loss = -wasserstein_loss(gan.discriminator(gen_images, subject_ids), is_real) # negative for real loss, since aiming to minimise
         g_loss.backward()
         optimizer_G.step()
@@ -94,10 +93,31 @@ for epoch in range(num_epochs):
         # Clip discriminator weights (important for stability in WGAN)
         for param in gan.discriminator.parameters():
             param.data.clamp_(-0.01, 0.01)
+     
+        # Validation loop
+        gan.eval()  # Set model to evaluation mode
+        total_d_val_loss = 0.0
 
+        with torch.no_grad():  # Disable gradient computation during validation
+            for (val_filtered_images, val_unfiltered_images, val_subject_ids) in val_dataloader:
+                
+                val_is_real = Variable(torch.ones(len(val_unfiltered_images), 1))
+                val_is_fake = Variable(torch.zeros(len(val_unfiltered_images), 1))
+                val_gen_images = gan.generator(val_filtered_images, val_subject_ids)
+                
+                val_real_loss = -wasserstein_loss(gan.discriminator(val_unfiltered_images, val_subject_ids), val_subject_ids) # negative for real loss, since aiming to minimise
+                val_fake_loss = wasserstein_loss(gan.discriminator(val_gen_images.detach(), val_subject_ids), val_is_fake)
+                val_d_loss = (val_real_loss + val_fake_loss) / 2
+                total_d_val_loss += val_d_loss.item()
+                
+        # Calculate and print average validation loss
+        val_avg_d_loss = total_d_val_loss / len(val_dataloader)
+    
     if epoch % 2 == 0:
         plt.imsave(f'gen_epoch_{epoch:02d}.png', gen_images[0,:,:,:,45].detach().numpy()[0], cmap='gray')
     print(
-        "[Epoch %d/%d] [D loss: %f] [G loss: %f]"
-        % (epoch, num_epochs, d_loss.item(), g_loss.item())
+        "[Epoch %d/%d] [train - D loss: %f] [train - G loss: %f] [val - D loss: %f]"
+        % (epoch, num_epochs, d_loss.item(), g_loss.item(), val_avg_d_loss.item())
     )
+
+   
