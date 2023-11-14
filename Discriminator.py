a4bd871fe8f256fe11db9403d1ef2ef615c92f33
sophmrtn <sophiedarrouxmartin@gmail.com>
2023-11-14 09:50:01 +0000

    reduce model size and add subject id info

--- Discriminator.py
+++ Discriminator.py
@@ -3,31 +3,34 @@ import torch.nn as nn
 
 class Discriminator(nn.Module):
     def __init__(self, 
-                 #image_size: tuple=(64,64,64), 
                  img_channels: int = 1, 
                  num_subjects: int = 15) -> None:
         
         super(Discriminator, self).__init__()
-        self.image_branch = nn.Sequential(
-            nn.Conv3d(img_channels, 64, kernel_size=4, stride=2, padding=1),
+        self.model = nn.Sequential(
+            nn.Conv3d(img_channels + num_subjects, 32, kernel_size=5, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv3d(64, 128, kernel_size=4, stride=2, padding=1), 
+            nn.Dropout3d(0.25),
+            nn.Conv3d(32, 64, kernel_size=5, stride=2, padding=1), 
+            nn.BatchNorm3d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout3d(0.25),
+            nn.Conv3d(64, 128, kernel_size=5, stride=2, padding=1),
             nn.BatchNorm3d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv3d(128, 256, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm3d(256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv3d(256, 1, kernel_size=4, stride=2, padding=1),
+            nn.Dropout3d(0.25),
+            nn.Conv3d(128, 1, kernel_size=5, stride=2, padding=1),
         )
-        # self.condition_embedding = nn.Embedding(num_subjects, 256)
-        # self.condition_linear = nn.Linear(5, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x, subject_ids):
-        x = self.image_branch(x)
-        # condition = self.condition_embedding(subject_ids)
-        # condition = self.condition_linear(condition)
+        subject_ids = subject_ids.view(subject_ids.size(0), subject_ids.size(1), 1, 1, 1)
+        subject_ids = subject_ids.expand(-1, -1, x.size(2), x.size(3), x.size(4))
+
+        # Concatenate subject_ids with the input x
+        x = torch.cat([x, subject_ids], dim=1)
+
+        x = self.model(x)
         x = x.view(x.size(0), -1)
-        # x = torch.cat([x, condition], dim=1)
         x = self.sigmoid(x)
         return x
